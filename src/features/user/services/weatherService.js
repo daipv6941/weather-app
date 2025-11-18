8c265a40a2f82d4fbc93530179d6ccbd04a3c476
seta-daipv-6941 <dai.phung@seta-international.vn>
2025-11-18 12:30:28 +0700

    Updates

--- src/features/user/services/weatherService.js
+++
@@ -1,6 +0,0 @@
-// src/features/user/services/weatherService.js
-export const getWeatherByCity = (city) => {
-  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-  const url = `${process.env.REACT_APP_WEATHER_API_URL}?q=${city}&units=metric&appid=${apiKey}`;
-  return url;
-};
